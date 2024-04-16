3adfe7b6ce64b18a1fce942bd1d74b9c46d219eb
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-04-16 15:18:20 +0500

    Get data from api

--- js/main.js
+++ js/main.js
@@ -1,7 +1,75 @@
 const elButton = document.querySelector('.site-header__dark-mode-button')
+// ul element index-countries-list
+const elCountriesList = document.querySelector('.countries__list');
+// search input element
+const elSearchInput = document.querySelector('.filters__search-input');
 
+// DARK MODE FUNCTION
 if(elButton) {
   elButton.addEventListener('click', () => {
     document.querySelector('body').classList.toggle('dark-mode')
+  })
+}
+// get data function
+function getData(data) {
+
+  data.forEach(element => {
+    const countryItem = document.createElement('li')
+    countryItem.classList.add('countries__item')
+    countryItem.innerHTML = `
+    <div class="country__card">
+      <img class="country__flag-img" src="${element.flags.svg}" alt="${element.flags.alt}" width="264" height="160">
+      <div class="country__about">
+        <h3 class="country__name">${element.name.common}</h3>
+        <dl class="country__details">
+          <div class="country__details-item">
+            <dt class="country__definition-title">Population:</dt>
+            <dd class="country__definition-description">${element.population}</dd>
+          </div>
+          <div class="country__details-item">
+            <dt class="country__definition-title">Region:</dt>
+            <dd class="country__definition-description regionName">${element.region}</dd>
+          </div>
+          <div class="country__details-item">
+            <dt class="country__definition-title">Capital:</dt>
+            <dd class="country__definition-description">${element.capital}</dd>
+          </div>
+          </dl>
+        </div>
+      </div>
+    `
+    elCountriesList.appendChild(countryItem)
+  });
+}
+
+fetch('https://restcountries.com/v3.1/all', {
+  method: 'GET',
+  headers: {'Content-Type': 'application/json'},
+})
+  .then(response => {return response.json()})
+  .then(data => {
+    getData(data)
+  })
+  .catch(err => {
+    console.log('error', err);
+  })
+  .finally(() => {
+    loaderWrapper.style.display = 'flex'
+  })
+
+
+// Search input function
+if(elSearchInput) {
+  const countryName = document.getElementsByClassName('country__name')
+  elSearchInput.addEventListener('input', e => {
+    // e = preventDefault()
+
+    Array.from(countryName).forEach(country => {
+      if(country.innerText.toLowerCase().includes(elSearchInput.value.toLowerCase())) {
+        country.parentElement.parentElement.parentElement.style.display = "grid"
+      } else {
+        country.parentElement.parentElement.parentElement.style.display = "none"
+      }
+    })
   })
 }
